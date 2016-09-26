04c02bf4ca973d84151e24ff49bfc51562c06ce9
Alexey Kutepov <a.kutepov@voskhod.ru>
2016-09-26 13:55:11 +0300

    Outside property

--- src/main/java/ru/kutepov/controller/MainController.java
+++ src/main/java/ru/kutepov/controller/MainController.java
@@ -1,5 +1,6 @@
 package ru.kutepov.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class MainController {
+
+  @Value("${value:none}")
+  public String value;
+
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
+    model.addAttribute("property_value", value);
 		return "hello";
 	}
 }
--- src/main/webapp/WEB-INF/pages/hello.jsp
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 	<h1>${message}</h1>
+    <h3>${property_value}</h3>
 </body>
 </html>
